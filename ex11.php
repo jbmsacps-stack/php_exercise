d594c5e003b97777ca74b8036b5de5d969839285
Joshua Baskar <jbmsacps@gmail.com>
2026-08-13 22:54:19 +0530

    Refactor file listing with form and table display
    Updated HTML structure and added a form for displaying files.

--- ex11.php
+++ ex11.php
@@ -2,25 +2,41 @@
 <html>
 <body>
 
-<h2>File List Sorted by Last Modification Time</h2>
+<h2>Files Sorted by Last Modification Time</h2>
+
+<form method="post">
+    <button type="submit" name="display">Display Files</button>
+</form>
 
 <?php
 
-$files = scandir(".");
+if (isset($_POST['display'])) {
 
-foreach($files as $file)
-{
-    if(is_file($file))
-    {
-        $time[$file] = filemtime($file);
+    $files = scandir(".");
+
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            $time[$file] = filemtime($file);
+        }
     }
-}
 
-arsort($time);
+    arsort($time);
 
-foreach($time as $file => $date)
-{
-    echo $file." - ".date("d-m-Y H:i:s",$date)."<br>";
+    echo "<table border='1'>";
+
+    echo "<tr>";
+    echo "<th>File Name</th>";
+    echo "<th>Last Modified Time</th>";
+    echo "</tr>";
+
+    foreach ($time as $file => $date) {
+        echo "<tr>";
+        echo "<td>$file</td>";
+        echo "<td>" . date("d-m-Y h:i:s A", $date) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
 }
 
 ?>
