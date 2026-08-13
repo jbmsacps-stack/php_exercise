7eee9d7703f91d7e92e906aa99bc5b9e072671cd
Joshua Baskar <jbmsacps@gmail.com>
2026-08-13 22:20:05 +0530

    Update ex9.php

--- ex9.php
+++ ex9.php
@@ -4,25 +4,43 @@
 
 <h2>File Content Reversal</h2>
 
+<form method="post">
+
+    Enter Input File Name:
+    <input type="text" name="input_file" required><br><br>
+
+    Enter Output File Name:
+    <input type="text" name="output_file" required><br><br>
+
+    <input type="submit" value="Reverse File">
+
+</form>
+
 <?php
 
-$content = file_get_contents("input.txt");
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $input_file = $_POST["input_file"];
+    $output_file = $_POST["output_file"];
 
-$reverse = strrev($content);
+    $content = file_get_contents($input_file);
 
-file_put_contents("output.txt", $reverse);
+    $reverse = strrev($content);
 
-echo "<b>Original Content:</b><br>";
-echo $content;
+    file_put_contents($output_file, $reverse);
 
-echo "<br><br>";
+    echo "<b>Original Content:</b><br>";
+    echo $content;
 
-echo "<b>Reversed Content:</b><br>";
-echo $reverse;
+    echo "<br><br>";
 
-echo "<br><br>";
+    echo "<b>Reversed Content:</b><br>";
+    echo $reverse;
 
-echo "Reversed content has been saved in <b>output.txt</b>";
+    echo "<br><br>";
+
+    echo "Reversed content has been saved in <b>$output_file</b>";
+}
 
 ?>
 
