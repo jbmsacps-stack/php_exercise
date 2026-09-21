43e93442fa441851c7f0f3959aa5932662399ebf
Joshua Baskar <jbmsacps@gmail.com>
2026-09-21 18:56:47 +0530

    Simplify email extraction and display logic
    Refactor email display logic and remove redundant code.

--- ex13.php
+++ ex13.php
@@ -1,48 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>XML Email Extraction</title>
-</head>
 <body>
 
 <h2>XML Email Extraction</h2>
 
 <form method="post">
-    <button type="submit" name="display">Display Emails</button>
+    <button name="display">Display Emails</button>
 </form>
 
 <?php
-
 if (isset($_POST['display'])) {
-
     $xml = simplexml_load_file("email.xml");
 
     echo "<h3>Email Addresses</h3>";
+    echo "<table border='1' cellpadding='10' cellspacing='0'>
+            <tr><th>S.No</th><th>Name</th><th>Email Address</th></tr>";
 
-    echo "<table border='1' cellpadding='10' cellspacing='0'>";
-
-    echo "<tr>";
-    echo "<th>S.No</th>";
-    echo "<th>Name</th>";
-    echo "<th>Email Address</th>";
-    echo "</tr>";
-
-    $sno = 1;
-
-    foreach ($xml->email as $email) {
-
-        echo "<tr>";
-        echo "<td>" . $sno . "</td>";
-        echo "<td>" . $email->name . "</td>";
-        echo "<td>" . $email->address . "</td>";
-        echo "</tr>";
-
-        $sno++;
-    }
+    $i = 1;
+    foreach ($xml->email as $email)
+        echo "<tr><td>".$i++."</td><td>".$email->name."</td><td>".$email->address."</td></tr>";
 
     echo "</table>";
 }
-
 ?>
 
 </body>
