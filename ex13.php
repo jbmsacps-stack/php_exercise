cd42852a47dc298bc7c9af6b7e1e33c54521ba3c
Joshua Baskar <jbmsacps@gmail.com>
2026-09-21 18:48:46 +0530

    Enhance email display with table format
    Refactor email display to use a table format with serial numbers and names.

--- ex13.php
+++ ex13.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>XML Email Extraction</title>
+</head>
 <body>
 
 <h2>XML Email Extraction</h2>
 
+<form method="post">
+    <button type="submit" name="display">Display Emails</button>
+</form>
+
 <?php
 
-$xml = simplexml_load_file("email.xml");
+if (isset($_POST['display'])) {
 
-echo "<h3>Email Addresses</h3>";
+    $xml = simplexml_load_file("email.xml");
 
-foreach($xml->email as $email)
-{
-    echo $email."<br>";
+    echo "<h3>Email Addresses</h3>";
+
+    echo "<table border='1' cellpadding='10' cellspacing='0'>";
+    echo "<tr>";
+    echo "<th>S.No</th>";
+    echo "<th>Name</th>";
+    echo "<th>Email Address</th>";
+    echo "</tr>";
+
+    $sno = 1;
+
+    foreach ($xml->email as $email) {
+
+        echo "<tr>";
+        echo "<td>" . $sno . "</td>";
+        echo "<td>" . $email->name . "</td>";
+        echo "<td>" . $email->address . "</td>";
+        echo "</tr>";
+
+        $sno++;
+    }
+
+    echo "</table>";
 }
 
 ?>
