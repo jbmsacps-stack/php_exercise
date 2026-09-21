cf4284f94e8fb442841ae6ad827a71a0181eb2dc
Joshua Baskar <jbmsacps@gmail.com>
2026-09-21 19:34:10 +0530

    Refactor student mark sheet form and queries

--- ex12.php
+++ ex12.php
@@ -4,53 +4,41 @@
 
 <h2>Student Mark Sheet</h2>
 
+<form method="post">
+ID: <input name="id"><br><br>
+Name: <input name="name"><br><br>
+Tamil: <input name="tamil"><br><br>
+English: <input name="english"><br><br>
+Maths: <input name="maths"><br><br>
+
+<button name="insert">Insert</button>
+<button name="update">Update</button>
+<button name="delete">Delete</button>
+</form>
+
 <?php
+$c=mysqli_connect("localhost","root","","college");
+extract($_POST);
 
-$conn = mysqli_connect("localhost","root","","college");
+if(isset($_POST['insert']))
+    mysqli_query($c,"INSERT INTO student VALUES('$id','$name','$tamil','$english','$maths')") && print "Record Inserted";
 
-if(!$conn)
-{
-    die("Connection Failed");
-}
+if(isset($_POST['update']))
+    mysqli_query($c,"UPDATE student SET name='$name',tamil='$tamil',english='$english',maths='$maths' WHERE id='$id'") && print "Record Updated";
 
-// Insert
-mysqli_query($conn,"INSERT INTO student VALUES(1,'Raj',85)");
-echo "Record Inserted<br>";
+if(isset($_POST['delete']))
+    mysqli_query($c,"DELETE FROM student WHERE id='$id'") && print "Record Deleted";
 
-// Update
-mysqli_query($conn,"UPDATE student SET mark=95 WHERE id=1");
-echo "Record Updated<br>";
+$r=mysqli_query($c,"SELECT * FROM student");
 
-// Display
-echo "<h3>Student Records</h3>";
+echo "<h3>Student Records</h3>
+<table border='1' cellpadding='8'>
+<tr><th>ID</th><th>Name</th><th>Tamil</th><th>English</th><th>Maths</th></tr>";
 
-$result = mysqli_query($conn,"SELECT * FROM student");
-
-echo "<table border='1' cellpadding='8'>";
-echo "<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Mark</th>
-</tr>";
-
-while($row=mysqli_fetch_assoc($result))
-{
-    echo "<tr>";
-    echo "<td>".$row['id']."</td>";
-    echo "<td>".$row['name']."</td>";
-    echo "<td>".$row['mark']."</td>";
-    echo "</tr>";
-}
+while($x=mysqli_fetch_assoc($r))
+    echo "<tr><td>{$x['id']}</td><td>{$x['name']}</td><td>{$x['tamil']}</td><td>{$x['english']}</td><td>{$x['maths']}</td></tr>";
 
 echo "</table>";
-
-// Delete
-mysqli_query($conn,"DELETE FROM student WHERE id=1");
-echo "<br><br>Record Deleted";
-
-mysqli_close($conn);
-
 ?>
-
 </body>
 </html>
